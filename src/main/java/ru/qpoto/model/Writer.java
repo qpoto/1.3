cc8a9897e69da6d9718b3ae0c6f740940a05b6ba
qpoto <s.kondratev@r77.center-inform.ru>
2025-11-10 20:07:37 +0300

    - Реализация первого метода репозитория Writers

--- src/main/java/ru/qpoto/model/Writer.java
+++ src/main/java/ru/qpoto/model/Writer.java
@@ -1,6 +1,7 @@
 package ru.qpoto.model;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Writer {
     private Long id;
@@ -8,4 +9,68 @@ public class Writer {
     private String lastName;
     private List<Post> posts;
     private Status status = Status.ACTIVE;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Writer writer = (Writer) o;
+        return Objects.equals(id, writer.id) && Objects.equals(firstName, writer.firstName) && Objects.equals(lastName, writer.lastName) && Objects.equals(posts, writer.posts) && status == writer.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, posts, status);
+    }
+
+    @Override
+    public String toString() {
+        return "Writer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", posts=" + posts +
+                ", status=" + status +
+                '}';
+    }
 }
--- src/main/java/ru/qpoto/repository/impl/GsonWriterRepositoryImpl.java
+++ src/main/java/ru/qpoto/repository/impl/GsonWriterRepositoryImpl.java
@@ -1,11 +1,24 @@
 package ru.qpoto.repository.impl;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import ru.qpoto.model.Writer;
 import ru.qpoto.repository.WriterRepository;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class GsonWriterRepositoryImpl implements WriterRepository {
+    private final String path = "writers.json";
     @Override
     public Writer findById(Long id) {
         return null;
@@ -13,12 +26,33 @@ public class GsonWriterRepositoryImpl implements WriterRepository {
 
     @Override
     public List<Writer> findAll() {
-        return List.of();
+        if (!Files.exists(Paths.get(path))) {
+            return new ArrayList<>();
+        }
+        Gson gson = new Gson();
+        try (FileReader reader = new FileReader(path)) {
+            Type listType = new TypeToken<List<Writer>>(){}.getType();
+            List<Writer> writers = gson.fromJson(reader, listType);
+            return writers != null ? writers : new ArrayList<>();
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
     }
 
     @Override
-    public void save(Writer entity) {
+    public void save(Writer writer) {
+        List<Writer> writers = findAll();
+        Optional<Writer> lastWriter = writers.stream().max(Comparator.comparing(Writer::getId));
+        Long id = lastWriter.isPresent() ? lastWriter.get().getId() : Long.valueOf(0L);
+        writer.setId(++id);
+        writers.add(writer);
 
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try (FileWriter jsonFileWriter = new FileWriter(path)) {
+            gson.toJson(writers, jsonFileWriter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
